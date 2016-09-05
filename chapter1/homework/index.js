51c5cb15ee755e3c25559f0545e0b288c81a8f86
m-dubois <matthieu.dubois@gmail.com>
2016-09-05 14:27:28 +0200

    chapter 1 homework

--- chapter1/homework/index.js
+++ chapter1/homework/index.js
@@ -1,2 +1,24 @@
 var mongodb = require('mongodb');
 var movies = require('./movies');
+
+var dbInterface = require('./interface');
+
+
+var uri = 'mongodb://localhost:27017/example';
+
+mongodb.MongoClient.connect(uri, function(error, db) {
+	if (error) {
+		console.log(error);
+		process.exit(1);
+	}
+
+	var doc = { title: 'Rogue One', year: 2016, director: 'Gareth Edwards' };
+	dbInterface.insert(db, doc, function(error) {
+      if (error) {
+        console.log(error);
+        process.exit(1);
+      }
+      console.log('all ok');
+      process.exit(0);
+	});
+});
